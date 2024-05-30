1651bbdbc820110d1e0c7754f9d61f06d6221b26
Lucas Roque Beckhauser <127861163+LucasRBeckhauser@users.noreply.github.com>
2024-05-30 15:51:20 -0300

    correção button home

--- src/components/apresentacao/Apresentacao.jsx
+++ src/components/apresentacao/Apresentacao.jsx
@@ -6,7 +6,7 @@ export default function Apresentacao() {
     const navigate = useNavigate();
 
     const handleNavigate = () => {
-        navigate('/projetos');
+        navigate('/colaboradores');
     }
 
     return (
