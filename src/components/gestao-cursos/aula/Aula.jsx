7739ad256105e84a562277f0542c089674c5d70c
Lucas Ronchi <lucas0headshot@gmail.com>
2024-06-24 20:35:15 -0300

    feat: components
    Adicionar componentes principais do módulo de Gestão de Cursos.

---
+++ src/components/gestao-cursos/aula/Aula.jsx
@@ -0,0 +1,14 @@
+/**
+ * @description Página de aula
+ *
+ * @author @lucas0headshot
+ *
+ * @return {React.JSX.Element}
+ */
+const Aula = () => {
+    return (
+        <div>Aula</div>
+    )
+}
+
+export default Aula
