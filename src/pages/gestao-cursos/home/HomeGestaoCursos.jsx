09adec5da0bf948480fc49356f4c839bc6fd4676
Lucas Ronchi <lucas0headshot@gmail.com>
2024-07-04 21:59:01 -0300

    refactor: HomeGestaoCursos
    Alterar do componente Home p/ HomeGestaoCursos.

--- src/pages/gestao-cursos/home/HomeGestaoCursos.jsx
+++ src/pages/gestao-cursos/home/HomeGestaoCursos.jsx
@@ -1,14 +1,14 @@
 /**
  * @description Home do Módulo de Gestão de Cursos.
  *
- * @author @lucas0headshot
+ * @author Lucas Ronchi <@lucas0headshot>
  *
- * @return {React.JSX.Element}
+ * @return {React.ReactElement}
  */
-const Home = () => {
+const HomeGestaoCursos = () => {
     return (
         <div>Bem-vindo ao módulo de Gestão de Cursos</div>
     )
 }
 
-export default Home
+export default HomeGestaoCursos;
