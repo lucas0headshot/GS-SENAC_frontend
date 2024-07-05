b0a4534ed08818b152bd3641a65224e4926d6849
Lucas Ronchi <lucas0headshot@gmail.com>
2024-07-04 22:24:27 -0300

    feat: Routes
    Adicionar rotas do nosso módulo.

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,38 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/partials/footer/Footer';
 import Header from './components/partials/header/Header';
-import Home from './pages/home/home';
+import Home from './pages/home/Home';
 import Colaboradores from './pages/colaboradores/Colaboradores';
 import Demissao from './pages/demissao/Demissao';
 import Admissao from './pages/admissao/Admissao';
+import HomeGestaoCursos from './pages/gestao-cursos/home/HomeGestaoCursos';
+import Cursos from './pages/gestao-cursos/cursos/Cursos';
+import Aulas from './pages/gestao-cursos/aulas/Aulas';
+import Inscricoes from './pages/gestao-cursos/inscricoes/Inscricoes';
+import Materias from './pages/gestao-cursos/materias/Materias';
 
 
 export default function App() {
+  const GESTAO_CURSO_PREFIX = '/gestao-cursos';
+
   return (
     <>
       <BrowserRouter>
-      <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="colaboradores" element ={<Colaboradores/>}/>
-          <Route path="admissao" element = {<Admissao/>} /> 
-          <Route path= "demissao" element = {<Demissao/>}/>
-        </Routes>
-      <Footer />
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="colaboradores" element ={<Colaboradores/>}/>
+            <Route path="admissao" element = {<Admissao/>} /> 
+            <Route path= "demissao" element = {<Demissao/>}/>
+
+            <Route path={`${GESTAO_CURSO_PREFIX}/`} element={<HomeGestaoCursos />}>
+              <Route path="cursos" element={<Cursos />} />
+              <Route path="aulas" element={<Aulas />} />
+              <Route path="inscricoes" element={<Inscricoes />} />
+              <Route path="materias" element={<Materias />} />
+            </Route>
+          </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   )
