f84aefb1b81440bf9f946485838503b5bbfc9ebc
Lucas Ronchi <lucas0headshot@gmail.com>
2024-07-07 21:33:42 -0300

    feat: Page404
    Adicionar componente p/ representar a pág de um HTTP 404.

---
+++ src/pages/Page404.jsx
@@ -0,0 +1,14 @@
+/**
+ * @description Página 404
+ *
+ * @author Lucas Ronchi <@lucas0headshot>
+ *
+ * @return {JSX.Element}
+ */
+const Page404 = () => {
+    return (
+        <div>Ih rapaiz... não encontramos esta página :/</div>
+    )
+}
+
+export default Page404
