1585bac9236d48703ed10d4975bcb57c70ea0ad8
Lucas Ronchi <lucas0headshot@gmail.com>
2024-07-09 02:32:09 -0300

    refactor: Footer
    Adicionar lógica p/ exibir sempre o ano atual.

--- src/components/partials/footer/Footer.jsx
+++ src/components/partials/footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <>
             <footer className={styles.rodape}>
-                <p>© Portal Rh 2024.</p>
+                <p>© Portal GS {new Date().getFullYear()}.</p>
             </footer>
         </>
     )
