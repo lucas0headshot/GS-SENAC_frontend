45a62cee91cff250eaef73b856576fa70de3ab77
Lucas Ronchi <lucas0headshot@gmail.com>
2024-07-10 04:37:15 -0300

    refactor: Inscricao
    Adicionar hook "carregando".
    Adicionar validação em alguns campos.
    Adicionar classes do BS.

--- src/pages/gestao-cursos/inscricoes/Inscricao.jsx
+++ src/pages/gestao-cursos/inscricoes/Inscricao.jsx
@@ -5,10 +5,17 @@ import { Col, Container, Row, Card, Form, Button } from 'react-bootstrap';
 import SelectFuncionario from '../../../components/gestao-cursos/curso/SelectFuncionario'
 import SelectCurso from '../../../components/gestao-cursos/curso/SelectCurso';
 
-
+/**
+ * @description Página de inscrição
+ *
+ * @author Juan Carlos <@JuannCarloss>
+ *
+ * @return {React.Component}
+ */
 const Inscricao = () => {
     const { id } = useParams();
     const navigate = useNavigate();
+    const [carregando, setCarregando] = useState(false);
     const [formData, setFormData] = useState({
         inscrito: { id: 0 },
         data: '',
@@ -24,13 +31,17 @@ const Inscricao = () => {
     }, [id]);
 
     const fetchInscricao = async () => {
-        Axios.get(`http://localhost:8080/api/inscricoes/${id}`)
+        setCarregando(true);
+        await Axios.get(`http://localhost:8080/api/inscricoes/${id}`)
             .then(response => {
                 setFormData(response.data);
             })
             .catch(err => {
                 console.error('Erro ao realizar GETID em inscricao:', err);
             })
+            .finally(() => {
+                setCarregando(false);
+            });
     };
 
     const handleChange = (e) => {
@@ -64,8 +75,6 @@ const Inscricao = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        formData.inscrito.id = parseInt(formData.inscrito.id);
-        formData.curso.id = parseInt(formData.curso.id);
         try {
             if (id) {
                 await Axios.put(`http://localhost:8080/api/inscricoes/${id}`, formData);
@@ -81,66 +90,61 @@ const Inscricao = () => {
 
 
     return (
-        <Container>
-            <Row className="vh-100 d-flex justify-content-center align-items-center">
-                <Col md={10} lg={8} xs={12}>
-                    <Card className="shadow">
-                        <Card.Header>
-                            <Card.Title className='text-uppercase'>{id ? 'Editar' : 'Cadastrar'}Inscrição</Card.Title>
-                        </Card.Header>
-                        <Card.Body>
-                            <div className="mb-3 mt-4">
-                                <div className="mt-3">
-                                    <Form>
-                                        <Row className="mb-2">
-                                            <Form.Group controlId="data" as={Col} className="mb-2">
-                                                <Form.Label>Data</Form.Label>
-                                                <Form.Control
-                                                    type="date"
-                                                    name="data"
-                                                    value={formData.data}
-                                                    onChange={handleChange}
-                                                    required
-                                                />
-                                            </Form.Group>
-                                        </Row>
-                                        <Row className="mb-2">
-                                            <SelectFuncionario 
-                                            controlId="inscrito" 
-                                            className="mb-2" 
-                                            label="Inscrito" 
-                                            name="inscrito" 
-                                            value={formData.inscrito.id}
-                                            onChange={handleChange}/>
-                                            <Form.Group controlId="valor" as={Col} className="mb-2">
-                                                <Form.Label>Valor</Form.Label>
-                                                <Form.Control type="number" name="valor" required value={formData.valor} onChange={handleChange}/>
-                                            </Form.Group>
-                                        </Row>
-                                        <Row className="mb-2">
-                                            <SelectCurso 
-                                            controlId='curso'
-                                            className='mb-2'
-                                            label='Curso'
-                                            name='curso'
-                                            value={formData.curso.id}
-                                            onChange={handleChange}
-                                             />
-                                        </Row>
-                                    </Form>
-                                </div>
-                            </div>
-                        </Card.Body>
-                        <Card.Footer className="justify-content-end d-flex gap-2">
-                            <Button variant="primary" type="submit" onClick={handleSubmit}>Salvar</Button>
-                            <Button variant="secondary" type="button" onClick={() => {
-                                navigate('/gestao-cursos/inscricoes')
-                            }}>Cancelar</Button>
-                        </Card.Footer>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
+        <Col md={10} lg={8} xs={12}>
+            {carregando ? (
+                <div className="text-center fs-3">Carregando...</div>
+            ) : (
+                <Card className="shadow">
+                    <Card.Header className="bg-primary text-white">
+                        <Card.Title className='text-uppercase'>{id ? 'Editar' : 'Cadastrar'} Inscrição</Card.Title>
+                    </Card.Header>
+                    <Card.Body>
+                        <Form className="d-flex flex-column gap-2">
+                            <Row className="gap-2">
+                                <Form.Group controlId="data" as={Col}>
+                                    <Form.Label>Data</Form.Label>
+                                    <Form.Control
+                                        type="date"
+                                        name="data"
+                                        min={new Date()}
+                                        value={formData.data}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </Form.Group>
+                            </Row>
+                            <Row className="gap-2">
+                                <SelectFuncionario
+                                controlId="inscrito"
+                                label="Inscrito"
+                                name="inscrito"
+                                value={formData.inscrito.id || 0}
+                                onChange={handleChange}/>
+                                <Form.Group controlId="valor" as={Col}>
+                                    <Form.Label>Valor</Form.Label>
+                                    <Form.Control type="number" name="valor" required value={formData.valor || 0} min={0} onChange={handleChange}/>
+                                </Form.Group>
+                            </Row>
+                            <Row className="gap-2">
+                                <SelectCurso
+                                controlId='curso'
+                                label='Curso'
+                                name='curso'
+                                value={formData.curso.id || 0}
+                                onChange={handleChange}
+                                    />
+                            </Row>
+                            <Card.Footer className="justify-content-end d-flex gap-2 bg-body">
+                                <Button variant="primary" type="submit" onClick={handleSubmit}>Salvar</Button>
+                                <Button variant="secondary" type="button" onClick={() => {
+                                    navigate('/gestao-cursos/inscricoes')
+                                }}>Cancelar</Button>
+                            </Card.Footer>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            )}
+        </Col>
     )
 }
 
