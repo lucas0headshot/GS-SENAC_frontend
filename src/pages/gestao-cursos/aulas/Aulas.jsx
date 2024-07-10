fa26d88f1ad8db09dc562f712ab8a4fe030efb8f
Lucas Ronchi <lucas0headshot@gmail.com>
2024-07-10 04:07:01 -0300

    refactor: Aulas
    Adicionar hook "carregando".
    Adicionar validação dados.
    Adicionar Dropdown.

--- src/pages/gestao-cursos/aulas/Aulas.jsx
+++ src/pages/gestao-cursos/aulas/Aulas.jsx
@@ -1,17 +1,28 @@
 import { useEffect, useState } from 'react';
 import Axios from 'axios';
-import { Button, Container, Table } from 'react-bootstrap';
+import { Button, Row, Table, Col, Dropdown } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import API_PATH from '../../../../API_PATH'; 
+import API_PATH from '../../../../API_PATH';
+import getDayFromDate from '../../../components/helpers/data/getDayFromDate';
 
+/**
+ * @description Página de aulas
+ *
+ * @author Gabriel Zomer <@Carrerogabriel>
+ *
+ * @return {React.Component}
+ */
 const Aulas = () => {
     const [aulas, setAulas] = useState([]);
+    const [carregando, setCarregando] = useState(false);
     const navigate = useNavigate();
 
     const fetchData = async () => {
         try {
+            setCarregando(true);
             const response = await Axios.get(`${API_PATH}aulas`);
             setAulas(response.data.content);
+            setCarregando(false);
         } catch (err) {
             console.error('Erro ao buscar aulas:', err);
         }
@@ -24,38 +35,63 @@ const Aulas = () => {
     const handleDelete = async (aula) => {
         try {
             await Axios.delete(`${API_PATH}aulas/${aula.id}`);
-            fetchData(); 
+            fetchData();
         } catch (err) {
             console.error('Erro ao excluir aula:', err);
         }
     };
 
+    const handleCadastrar = () => {
+        navigate('/gestao-cursos/aulas/criar');
+    }
+
     return (
-        <Container className="mt-5">
-            <Button variant="primary" onClick={() => navigate('/gestao-cursos/aulas/criar')}>Criar</Button>
-            <Table striped bordered hover responsive>
-                <thead>
-                    <tr>
-                        <th>Matéria</th>
-                        <th>Dia</th>
-                        <th>Professor</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {aulas.map((aula, i) => (
-                         <tr key={aula.id} >
-                            <td>{aula.nome}</td>
-                            <td>{aula.dia}</td>
-                            <td>{aula.nomeProfessor}</td>
-                            <td className="justify-content-center d-flex gap-2">
-                                <Button variant="warning" onClick={() => navigate(`/gestao-cursos/aulas/editar/${aula.id}`)}>Editar</Button>
-                                <Button variant="danger" onClick={() => window.confirm("Deseja realmente excluir esta aula?") ? handleDelete(aula) : null}>Excluir</Button>
-                            </td>
+        <Row>
+            <Row className="justify-content-between mb-3">
+                <Col md={1}>
+                    <Button variant="success" onClick={handleCadastrar}>Cadastrar</Button>
+                </Col>
+            </Row>
+
+            {carregando ? (
+                <div className="text-center fs-3">Carregando...</div>
+            ) : (
+                <Table striped bordered hover responsive>
+                    <thead>
+                        <tr>
+                            <th>Matéria</th>
+                            <th>Dia</th>
+                            <th>Professor</th>
+                            <th>Ações</th>
                         </tr>
-                    ))}
-                </tbody>
-            </Table>
-        </Container>
+                    </thead>
+                    <tbody>
+                        {!aulas.length ? (
+                            <tr>
+                                <td colSpan="6" className="text-center">Nenhuma aula encontrada</td>
+                            </tr>
+                        ) : (
+                            aulas.map((aula, i) => (
+                                <tr key={i} >
+                                    <td>{aula.materia.nome}</td>
+                                    <td>{getDayFromDate(aula.dia)}</td>
+                                    <td>{aula.professor.nome}</td>
+                                    <td className="text-center">
+                                        <Dropdown>
+                                            <Dropdown.Toggle variant="secondary" id={`dropdown-aula-${aula.id}`}>Ações</Dropdown.Toggle>
+                                            <Dropdown.Menu>
+                                                <Dropdown.Item onClick={() => navigate(`/gestao-cursos/aulas/editar/${aula.id}`)}>Editar</Dropdown.Item>
+                                                <Dropdown.Item onClick={() => window.confirm("Deseja realmente excluir esta aula?") ? handleDelete(aula) : null}>Excluir</Dropdown.Item>
+                                            </Dropdown.Menu>
+                                        </Dropdown>
+                                    </td>
+                                </tr>
+                            ))
+                        )}
+                    </tbody>
+                </Table>
+            )}
+        </Row>
     );
 };
 
